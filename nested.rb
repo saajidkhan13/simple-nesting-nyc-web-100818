0c24a68bd052ed8b5d0f40ec60a8bb45a9084109
Saajid Khan <saajid.khan13@stjohns.edu>
2018-09-26 22:37:42 +0000

    Done.

--- nested.rb
+++ nested.rb
@@ -16,7 +16,7 @@ def hopper
         }
      }
 
-
+return programmer_hash[:grace_hopper]
 end
 
 def alan_kay_is_known_for
@@ -37,6 +37,7 @@ def alan_kay_is_known_for
           :languages => ["C"]
         }
      }
+     return programmer_hash[:alan_kay][:known_for]
 end
 
 def dennis_ritchies_language
@@ -55,6 +56,7 @@ def dennis_ritchies_language
           :languages => ["C"]
         }
      }
+     return programmer_hash[:dennis_ritchie][:languages][0]
 end
 
 def adding_matz
@@ -79,8 +81,12 @@ def adding_matz
           :languages => ["C"]
         }
      }
-
-    
+programmer_hash[:yukihiro_matsumoto] = {
+  :known_for => "Ruby",
+  :languages => ["LISP", "C"]
+}
+    new_array = programmer_hash.to_a 
+    return Hash[[new_array[3]]]
 end
 
 def changing_alan
@@ -101,7 +107,9 @@ def changing_alan
      }
      #change what Alan Kay is :known_for to the value of the alans_new_info variable. 
      alans_new_info = "GUI"
-     
+     programmer_hash[:alan_kay][:known_for] = alans_new_info
+     secondArray = programmer_hash.to_a 
+     return Hash[[secondArray[1]]]
      
 end
 
@@ -121,5 +129,8 @@ def adding_to_dennis
           :languages => ["C"]
         }
      }
+     programmer_hash[:dennis_ritchie][:languages] << "Assembly"
+     thirdArray = programmer_hash.to_a
+     return Hash[[thirdArray[2]]]
 
 end
